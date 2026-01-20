40bf2653c18006f74626bd0d25e69db5b7ee0ee9
Ragnar Kazunin <ragnar.kazunin@epita.fr>
2026-01-20 22:50:50 +0100

    Version 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from object import *
+from object import Circle
 
 import math
 import OpenGL.GL as GL
@@ -6,8 +6,12 @@ import OpenGL.GLUT as GLUT
 
 # Variables
 screen_size = [500, 500]
-position = [screen_size[0] // 2, screen_size[1] // 2]
-velocity = [15, 0]
+
+x2 = Circle(250,250,25,64)
+x2.InitConditions([250,250],[-12,20])
+
+x3 = Circle(250,250,25,64)
+x3.InitConditions([250,250],[12,-20])
 
 def display():
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
@@ -15,20 +19,15 @@ def display():
     GL.glLoadIdentity()
     
     # Draw Primitives
-    Circle(position[0],position[1],25,64)
+    x2.Draw()
+    x3.Draw()
 
     GL.glFlush()
 
 def update(value):
-    position[0] = position[0] + velocity[0]
-    position[1] = position[1] + velocity[1]
 
-    # Gravity Force
-    velocity[1] = velocity[1] -9.81 / 20
-
-    # Border Collision Force
-    if (position[0] <= 0) or (position[0] >= screen_size[0]): velocity[0] = velocity[0] * -0.95
-    if (position[1] <= 0) or (position[1] >= screen_size[1]): velocity[1] = velocity[1] * -0.95
+    x2.ApplyForces()
+    x3.ApplyForces()
 
     # Advance Frame
     GLUT.glutPostRedisplay()
--- object.py
+++ object.py
@@ -1,20 +1,64 @@
-from main import screen_size
+screen_size = [500, 500]
 
 import math
 import OpenGL.GL as GL
 import OpenGL.GLUT as GLUT
 
 class Circle:
+
     def __init__(self,centerX,centerY,radius,resolution):
+        
+        self.position = [0, 0]
+        self.velocity = [0, 0]
 
+        self.centerX = centerX
+        self.centerY = centerY
+        self.radius = radius
+        self.resolution = resolution
+    
+    def Draw(self):
+        
         GL.glBegin(GL.GL_TRIANGLE_FAN)
-        GL.glVertex2d(centerX,centerY)
+        GL.glVertex2d(self.position[0],self.position[1])
 
-        for i in range(resolution + 1):
-            angle = 2.0 * math.pi * (i / resolution)
-            x = centerX + math.cos(angle) * radius
-            y = centerY + math.sin(angle) * radius
+        for i in range(self.resolution + 1):
+            angle = 2.0 * math.pi * (i / self.resolution)
+            x = self.position[0] + math.cos(angle) * self.radius
+            y = self.position[1] + math.sin(angle) * self.radius
 
             GL.glVertex2d(x,y)
         
         GL.glEnd()
+
+
+    def ApplyForces(self):
+
+        self.position[0] = self.position[0] + self.velocity[0]
+        self.position[1] = self.position[1] + self.velocity[1]
+        
+        # Gravity Force
+        self.velocity[1] = self.velocity[1] -9.81 / 20
+
+        restitution = 0.95
+        # Border Collision Force
+
+        # X
+        if self.position[0] - self.radius < 0:
+            self.position[0] = self.radius
+            self.velocity[0] = abs(self.velocity[0]) * restitution
+        elif self.position[0] + self.radius > screen_size[0]:
+            self.position[0] = screen_size[0] - self.radius
+            self.velocity[0] = -abs(self.velocity[0]) * restitution
+
+        # Y
+        if self.position[1] - self.radius < 0:
+            self.position[1] = self.radius
+            self.velocity[1] = abs(self.velocity[1]) * restitution
+        elif self.position[1] + self.radius > screen_size[1]:
+            self.position[1] = screen_size[1] - self.radius
+            self.velocity[1] = -abs(self.velocity[1]) * restitution
+
+    def InitConditions(self,init_position,init_velocity):
+
+        self.position = init_position
+        self.velocity = init_velocity
